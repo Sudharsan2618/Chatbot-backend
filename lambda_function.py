6420d92bd7e087b2b5c1e792f9e988b93953aa96
Sudharsan D S <105963347+Sudharsan2618@users.noreply.github.com>
2024-11-27 14:36:59 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -85,4 +85,5 @@ def process_query():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=int(os.getenv("PORT", 5000)))
+
